8a1cb876905099e4304ac25450d98223b024889c
Sches <naste4ka11.11@gmail.com>
2018-09-20 18:08:04 +0300

    Solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var reg = / /g;
+  equation = equation.replace(reg, '');
+  var position1 = equation.indexOf("*", 0);
+  var a = equation.substring(0, position1);
+  position1 += 4;
+  var position2 = equation.indexOf("*", position1);
+  var b = equation.substring(position1, position2);
+  position2 += 2;
+  var c = equation.substring(position2);
+
+  var d = Math.round(Math.sqrt(b*b - 4*a*c));
+  var x1 = ((-1)*b + d)/(2*a);
+  var x2 = ((-1)*b - d)/(2*a);
+  var x = [];
+  x.push(x1, x2);
+  return x.sort((a, b) => a-b);
 }
